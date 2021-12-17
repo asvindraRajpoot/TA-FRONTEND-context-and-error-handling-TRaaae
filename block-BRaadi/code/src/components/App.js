bb9c858fc82e0dbb83bd00aed9639f99de6f6144
asvindra <aswin1399904@gmail.com>
2021-12-17 20:14:47 +0530

    completed Assignment: Error Boundary

--- block-BRaadi/code/src/components/App.js
+++ block-BRaadi/code/src/components/App.js
@@ -1,14 +1,19 @@
 import React from "react";
 import Header from "./Header";
 import Main from "./Main";
-
+import { SectionOneErrorBoundary } from "./SectionOneErrorBoundry";
 class App extends React.Component {
+  state = {
+    isDarkMode: false,
+  }
   render() {
     let { isDarkMode } = this.state;
     return (
       <>
         <Header isDarkMode={isDarkMode} />
+
         <Main isDarkMode={isDarkMode} />
+
       </>
     );
   }
--- block-BRaadi/code/src/components/Main.js
+++ block-BRaadi/code/src/components/Main.js
@@ -1,14 +1,19 @@
 import Location from "./Location";
 import SectionOne from "./SectionOne";
 import SectionTwo from "./SectionTwo";
+import { SectionOneErrorBoundary } from "./SectionOneErrorBoundry";
+
 
 function Main() {
   return (
     <section id="section-one">
       <div className="grid">
-        <SectionOne />
-        <SectionTwo />
-        <Location />
+        <SectionOneErrorBoundary>
+          <SectionOne />
+          <SectionTwo />
+          <Location />
+        </SectionOneErrorBoundary>
+
       </div>
     </section>
   );
